5d8981d4146e2e51e26eef5531f7083528919cb1
surajtiwarii19 <surajkumartiwari1804@gmail.com>
2025-09-15 00:48:44 +0530

    Video4: const, let,var

---
+++ video4.js
@@ -0,0 +1,22 @@
+const accountId = 16237347
+let accountEmail = "abc@gmail.com"
+var accountpassword = "13435"
+let accountCity = "Rudrapur"
+let accountState;
+
+// accountId = 2
+accountEmail = "asdf@gmail.com"
+accountpassword = 1421325
+accountCity = "Noida"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountCity, accountState])
+
+/* 
+variables and constants
+
+const= it is a variable and cannot be changed
+var and let can be used to declare the variable but use let instead of var as it gets issue in block scope and function scope
+if you list a variable like accountState so i gets undefined value in it
+*/
