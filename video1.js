ed20dd4b0f72320dde5b730e33e6ae0f58350dce
surajtiwarii19 <surajkumartiwari1804@gmail.com>
2025-09-14 21:13:02 +0530

    video1: what is javascript

---
+++ video1.js
@@ -0,0 +1,4 @@
+                                // what is javascript
+
+
+// JavaScript is a high-level, dynamic, and interpreted programming language primarily used to create interactive and dynamic content on websites
