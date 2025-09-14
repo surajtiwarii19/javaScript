72fca9294eb8b67d6987934f85a301640e4231c6
surajtiwarii19 <surajkumartiwari1804@gmail.com>
2025-09-15 01:49:31 +0530

    video6: Conversion of Data Types

---
+++ video6.js
@@ -0,0 +1,38 @@
+let score = "352gsag";
+// let score = null;
+// let score = undefined;
+// let score = true;
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+
+
+// let isLoggedin = 1;
+// let isLoggedin = "Suraj";
+// let isLoggedin = "";
+
+let booleanIslogged = Boolean(isLoggedin)
+console.log(booleanIslogged)
+
+// let someNumber = 41
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+/*
+Conversion
+"33" = 33
+"352gsag = NaN"
+true = 1; false = 0;
+1 converted to boolean gets true or you write name("Suraj") gives true or you put empty("") gives false
+
+
+*/
