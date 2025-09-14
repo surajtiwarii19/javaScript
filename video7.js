5ad0e0b84d3b060bf6c2ec8da13c7aff0abfbba2
surajtiwarii19 <surajkumartiwari1804@gmail.com>
2025-09-15 02:20:57 +0530

    video7: Operators, pre and post increment,decrement

---
+++ video7.js
@@ -0,0 +1,44 @@
+let value = 3
+let negativeValue = -value
+console.log(negativeValue); //-3
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2**3)
+// console.log(2%3)
+
+let str1 = "Suraj" 
+let str2 = " tiwari"
+let str3 = str1 + str2
+console.log(str3); // Suraj tiwari
+
+console.log("1" + 8 + 0 + 4); // 1804
+console.log(1 + 8 + 0 + "4"); // 94
+console.log(1 + 8 - 6 * 4); // -15
+console.log(true); // true
+console.log(+true); // 1
+// console.log(true+); // Gives error
+console.log(+""); // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 3+3
+
+let gameScore = 100
+// gameScore++ ,
+// ++gameScore ,
+// gameScore-- ,
+// --gameScore
+console.log(gameScore);
+
+
+
+
+
+
+
+/*
+Operations
+
+*/
