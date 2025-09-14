8e164d57334f83921566a9fa022e681df596fd7e
surajtiwarii19 <surajkumartiwari1804@gmail.com>
2025-09-15 03:39:05 +0530

    video9: primitive and non primitive datatype

---
+++ video9.js
@@ -0,0 +1,47 @@
+// {Java script is a dynamically datatype Language}
+
+/*
+-- Primitive
+
+7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+*/
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn =false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId =Symbol("123")
+console.log("id"=='anotherId');
+
+const bigNumber = 2625652620865097n
+
+/*
+
+-- Non Primitive or Reference Datatype 
+
+Types : Array, Object, Functions
+
+*/
+
+const heros = ["Batman", "Spiderman", "Superman"]
+console.log(typeof Superman);
+
+
+let Object = {
+    name: "suraj",
+    age: 19,
+
+}
+
+
+const Myfunction = function(){
+    console.log("hello world");
+}
+console.log(typeof Myfunction);
+
+
