c8fff9e9ff59b9ef408d9496a97b6fdc2a02037c
surajtiwarii19 <surajkumartiwari1804@gmail.com>
2025-09-15 01:21:09 +0530

    video5: Data types

---
+++ video5.js
@@ -0,0 +1,39 @@
+"use strict"; //treat all code as newer version
+
+// alert("hello") //we are using nodejs not browser as it works on it
+
+console.log(3+4);
+console.log("Suraj");
+console.log(typeof age);
+console.log(typeof undefined);
+
+let name = "xyz"
+let age = 53
+let Loggedin = false
+let sex = null
+
+
+// number = 2 to power 53
+// bigint
+// string = " "
+// boolean = true/false
+// null = stand Alone value
+// undefined = 
+// symbol = unique
+
+// object 
+
+
+
+
+
+
+
+/*
+Data Types
+
+mdn web docs , Ecma script
+
+Always write code in a readable manner
+
+*/
