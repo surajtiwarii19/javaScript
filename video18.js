7fbad9755a6bb10ac176cd9848f0b3f93427ef62
surajtiwarii19 <surajkumartiwari1804@gmail.com>
2025-09-22 14:14:49 +0530

    video18: Objects part 3

---
+++ video18.js
@@ -0,0 +1,33 @@
+const course  = {
+    coursename: "js in hindi",
+    price: "421",
+    courseInstructor: "Hitesh",
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+console.log(courseInstructor);
+
+// const navbar = ({company}) = {
+
+// }
+
+// navbar(company = "hitesh")   //Object de-structuring
+
+// {
+//     name: "hitesh",
+//     "coursename": "js in hindi",
+//     "price"; "free",
+    
+// }
+
+[
+    {},
+    {},
+    {},
+]
+
+// https://jsonformatter.org/ //contains data structured values in objects and arrays
+
